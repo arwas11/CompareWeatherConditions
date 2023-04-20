3c9b1ff47e28270acb17f212bee61e38af1c8bb8
Arwa <arwamsharif@gmail.com>
2023-04-19 23:43:54 -0500

    data fetched and displayed

--- src/App.js
+++ src/App.js
@@ -1,37 +1,77 @@
 import { useState, useEffect } from "react";
-// import CurrentWeather from "./components/currentWeather/CurrentWeather";
 import Form from "./components/Form";
 
 export default function App() {
+  //CURRENT
   const [currentWeather, setCurrentWeather] = useState(null);
 
-  
+  const currentWeatherApiKey = 
 
-  const getWeather = async (searchTerm) => {
+  const getCurrentWeather = async (searchTerm) => {
     const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&APPID=${currentWeatherApiKey}&units=imperial`;
     try {
+      if (!searchTerm) {
+        setCurrentWeather(null)
+        return
+      }
       const response = await fetch(currentWeatherUrl);
       const currentWeatherData = await response.json();
-      console.log(currentWeatherData);
+      // console.log(currentWeatherData);
       setCurrentWeather(currentWeatherData);
-      console.log("this is currentWeatherData", currentWeatherData);
+      // console.log("this is currentWeatherData", currentWeatherData);
     } catch (err) {
       console.error(err);
     }
   };
 
-  // console.log("this is getweather()", getWeather());
+  //PAST
+  const [pastWeather, setpastWeather] = useState(null);
+
+  const getPastWeather = async (lat, lon, startDate, endDate) => {
+    const pastWeatherUrl = `https://archive-api.open-meteo.com/v1/archive?latitude=${lat}&longitude=-${lon}&start_date=${startDate}&end_date=${endDate}&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum,rain_sum,snowfall_sum,windspeed_10m_max,winddirection_10m_dominant&timezone=auto&temperature_unit=fahrenheit&windspeed_unit=mph`;
+
+    try {
+      const response = await fetch(pastWeatherUrl);
+      const pastWeatherData = await response.json();
+      console.log(
+        "this is past weather data before setting state",
+        pastWeatherData
+      );
+      //error handling
+      // if (pastWeatherData > 400) {
+      //   return <h1>Network response was not OK</h1>
+      // }
+      // if (!response.ok) {
+      //   throw new Error("Network response was not OK");
+      // }
+      setpastWeather(pastWeatherData);
+      console.log(
+        "this is past weather data after setting state",
+        pastWeatherData
+      );
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  // console.log("this is Current()", Current());
   //how to have no default???
   // useEffect(() => {
-  //   getWeather();
+  //   getPastWeather();
   // }, []);
 
   return (
     <div className="App">
-      <h1>Compare today's Weather with How it was in the Past</h1>
+      <h1>Compare Today's Weather To How It Was In The Past</h1>
       <div className="cards-div">
-        <Form citySearch={getWeather} currentWeather={currentWeather} />
+        <Form
+          citySearch={getCurrentWeather}
+          currentWeather={currentWeather}
+          getPastWeather={getPastWeather}
+          pastWeather={pastWeather}
+        />
       </div>
     </div>
   );
+  
 }
--- src/components/currentWeather/CurrentWeather.js
+++ src/components/currentWeather/CurrentWeather.js
@@ -1,25 +1,28 @@
-// import { useState } from "react";
+export default function CurrentWeather(props) {
+  console.log("this is currentweather state", props.currentWeather);
 
-export default function CurrentWeather({ currentWeather }) {
-  console.log("this is currentweather state", currentWeather);
-  // console.log(name)
-  
   try {
-    
     const loaded = () => {
-      const {
-        name,
-        wind,
-        rain,
-        snow,
-        main,
-        visibility,
-        clouds,
-        sys
-      } = currentWeather;
-      const icon = currentWeather.weather[0].icon;
+      const { name, wind, rain, snow, main, sys } =
+        props.currentWeather;
+      const icon = props.currentWeather.weather[0].icon;
       const iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
-  
+      const sunriseToUTC = new Date(sys.sunrise).toLocaleTimeString("en-US");
+      const sunsetToUTC = new Date(sys.sunset).toLocaleTimeString("en-US");
+
+      // showing all AM time!! need fixing
+      // const options = {
+      //   hour: "numeric",
+      //   minute: "numeric",
+      //   second: "numeric",
+      //   timeZone: props.pastWeather && props.pastWeather.timezone,
+      //   timeZoneName: "short",
+      // };
+      // const sunriseToUTC = new Intl.DateTimeFormat("en-AU", options).format(sys.sunrise)
+      // console.log("this is time", sunriseToUTC);
+
+      // const sunsetToUTC = new Intl.DateTimeFormat("en-AU", options).format(sys.sunset)
+      // console.log("this is time", sunsetToUTC);
       return (
         <>
           Current weather data
@@ -27,37 +30,35 @@ export default function CurrentWeather({ currentWeather }) {
             <i>City & Country Code</i>: {name}, {sys.country}
           </h3>
           <h3>
-            
-            Weather Description: "{currentWeather.weather[0].description}."
+            Weather Description:{" "}
+            <i>{props.currentWeather.weather[0].description}</i>
             <img src={iconUrl} alt="an icon describing the state of weather" />
           </h3>
-          <h3> Sunrise: {sys.sunrise} </h3>
-          <h3> Sunset: {sys.sunset} </h3>
-          <h3> Current Tempreture: {main.temp} </h3>
-          <h3> Feels Like: {main.feels_like} </h3>
-          <h3> Today's Tempreture(min): {main.temp_min}</h3>
-          <h3> Today's Tempreture(max): {main.temp_max}</h3>
-          <h3> Rain: {rain} </h3>
-          <h3> Snow {snow} </h3>
-          <h3> Clouds: {clouds.all}% </h3>
-          <h3> Humidity: {main.humidity} </h3>
-          <h3> Visibility: {visibility} </h3>
-          <h3> Wind Speed: {wind.speed} </h3>
-          <h3> Wind Direction: {wind.deg} </h3>
+          {/* add date now and correct time */}
+          <h3> Sunrise: {sunriseToUTC} </h3>
+          <h3> Sunset: {sunsetToUTC} </h3>
+          <h3> Current Temperature: {main.temp}°F </h3>
+          <h3> Feels Like: {main.feels_like}°F </h3>
+          {/* <h3> Today's Temperature(min): {main.temp_min}</h3>
+          <h3> Today's Temperature(max): {main.temp_max}</h3> */}
+          {/* api data is mm, need to make % */}
+          <h3> Rain : {rain ? rain : "No Rain For The Last 3 Hours"} </h3>
+          <h3> Snow: {snow ? snow : "No Snow For The Last 3 Hours"} </h3>
+          {/* <h3> Humidity: {main.humidity}% </h3>  */}
+          <h3> Wind Speed: {wind.speed} mph </h3>
+          <h3> Wind Direction: {wind.deg}° </h3>
         </>
       );
     };
-  
+
     //function to return loading JSX
     const loading = () => {
       return <h1>No Weather to Display</h1>;
     };
-  
+
     //Ternary operator will determine which functions JSX we will return
-    return currentWeather ? loaded() : loading();
-    
+    return props.currentWeather ? loaded() : loading();
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
-  
 }
